7db39b9637dfa0868abecd9a0c6ab00c46dcae40
hkg328 <hkg328@outlook.com>
2022-01-25 05:05:52 -0800

    complete

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,32 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.greeting === 'loadImages') {
+    let images = Array.from(document.querySelectorAll('.imgpage .imglist li'));
+    let rlt = [];
+    if (images) {
+      images.map((img) => {
+        let name = img.querySelector('a.imgitem-title')
+          ? img.querySelector('a.imgitem-title').innerText
+          : '';
+        let url = img.querySelector('img.main_img')
+          ? img.querySelector('img.main_img').src
+          : '';
+        // let url = img.querySelector('a.down')
+        //   ? img.querySelector('a.down').href
+        //   : '';
+        if (name && url) {
+          rlt.push({
+            name,
+            url,
+          });
+        }
+        return null;
+      });
+    }
+    console.log(images);
+    sendResponse({ images: rlt });
+  }
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from '../../assets/img/logo.svg';
-import Greetings from '../../containers/Greetings/Greetings';
+import React, { useState } from 'react';
 import './Popup.css';
 
 const Popup = () => {
+
+  const [ images, setImages ] = useState([]);
+  const [ downloading, setDownloading ] = useState(false);
+  const [progress, setProgress] = useState(0);
+
+  const loadImages = () => {
+    // get current tab data here.
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {greeting: "loadImages"}, function(response) {
+          // console.log(response.images);
+          setImages(response.images);
+      });
+    });
+  }
+
+  const getExtension = (url) => {
+    var extension = url.split('.').pop();
+    if (extension && extension.length <= 5) {
+      return extension;
+    } else {
+      return 'jpg';
+    }
+  }
+
+  const downloadImages = async () => {
+    setDownloading(true);
+    let i = 0;
+    for (let i = 0; i < images.length; i++) {
+      setProgress(100 / images.length * (i + 1));
+      let item = images[i]
+      try {
+        let response = await fetch(item.url)
+        let blob = await response.blob()
+        const blobURL = URL.createObjectURL(blob);
+        let downloadLink = document.createElement("a");
+
+        let extension = getExtension(item.url)
+        downloadLink.download = `${item.name}.${extension}`;
+        downloadLink.href = blobURL;
+        downloadLink.style.display = "none";
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+      } catch (e) {
+        console.log('we got error', e)
+      }
+    }
+    setDownloading(false);
+    alert('Download complete!')
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-      </header>
+      <h2 className="title">Baidu Image downloader</h2>
+      <div className="buttons">
+        <button onClick={loadImages}>Load Images</button>
+        <button onClick={downloadImages} disabled={downloading || images.length === 0}>Download Images</button>
+      </div>
+      <div className="progress" style={{ width: `${progress}%` }}></div>
+      <div className="images-list">
+      { images && images.map((image, index) => {
+        return <div key={index}>
+          <span className='index-of-image'>{ index + 1 }</span>
+          { image.name }
+          </div>
+      }) }
+      </div>
     </div>
   );
 };
